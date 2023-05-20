dd8789fdf4bf3ee4c807f03049114e6d029067b3
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-05-20 02:52:26 +0000

    [finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
   get '/homes/about' => 'homes#about' , as:'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 
 end
